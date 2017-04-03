b0c2a772e5bbf6c7b0d32b2da898daac2f021b1c
Heston Liebowitz <heston@matter.com>
2017-04-02 22:11:02 -0700

    Added status messages to trigger

--- dingdongditch/trigger.py
+++ dingdongditch/trigger.py
@@ -8,7 +8,9 @@ button = Button(settings.GPIO_INPUT_PIN)
 button.when_pressed = notifier.notify_recipients
 
 def run():
+    print('Running')
     try:
         signal.pause()
     except KeyboardInterrupt:
+        print('Done')
         return
