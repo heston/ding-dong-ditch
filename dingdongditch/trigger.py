ed9525f63cf415587fa4d25d495b13a1e3a92317
Heston Liebowitz <heston@matter.com>
2017-04-04 22:19:06 -0700

    Added trigger throttling
    Also use gevent.sleep to manage event loop

--- dingdongditch/trigger.py
+++ dingdongditch/trigger.py
@@ -1,16 +1,46 @@
-import signal
+import datetime
+import functools
 
+import gevent
 from gpiozero import Button
 
 from . import notifier, settings
 
+WINDOW = datetime.timedelta(seconds=15)
+
+
+def throttle(window):
+    def wrapper(func):
+        state = {
+            'last': None
+        }
+
+        @functools.wraps(func)
+        def inner():
+            now = datetime.datetime.now()
+            if state['last'] is None or now - state['last'] > window:
+                state['last'] = now
+                func()
+        return inner
+    return wrapper
+
+
+@throttle(WINDOW)
+def trigger():
+    print('Triggered!')
+    notifier.notify_recipients()
+
+
 button = Button(settings.GPIO_INPUT_PIN)
-button.when_pressed = notifier.notify_recipients
+button.when_pressed = trigger
+
 
 def run():
     print('Running')
+    print('Watching GPIO pin: ', settings.GPIO_INPUT_PIN)
     try:
-        signal.pause()
+        while True:
+            gevent.sleep(1)
     except KeyboardInterrupt:
-        print('Done')
+        print('Done\n')
         return
