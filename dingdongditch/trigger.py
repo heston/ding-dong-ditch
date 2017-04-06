b62c99a9b839226883c9f002ef59bf66ea0366b7
Heston Liebowitz <heston@matter.com>
2017-04-05 22:17:08 -0700

    Change print to log

--- dingdongditch/trigger.py
+++ dingdongditch/trigger.py
@@ -1,5 +1,6 @@
 import datetime
 import functools
+import logging
 
 import gevent
 from gpiozero import Button
@@ -7,6 +8,7 @@ from gpiozero import Button
 from . import notifier, settings
 
 WINDOW = datetime.timedelta(seconds=15)
+logger = logging.getLogger(__name__)
 
 
 def throttle(window):
@@ -15,19 +17,25 @@ def throttle(window):
             'last': None
         }
 
+        logger.debug('function %s throttled at %s', func, window)
+
         @functools.wraps(func)
         def inner():
             now = datetime.datetime.now()
-            if state['last'] is None or now - state['last'] > window:
+            elapsed = now - state['last']
+            if state['last'] is None or elapsed > window:
                 state['last'] = now
+                logger.debug('throttle executing: %s', func)
                 func()
+            else:
+                logger.debug('throttle skipping: %s. Elapsed window: %s', func, elapsed)
         return inner
     return wrapper
 
 
 @throttle(WINDOW)
 def trigger():
-    print('Triggered!')
+    logger.info('Trigger activated')
     notifier.notify_recipients()
 
 
@@ -36,11 +44,11 @@ button.when_pressed = trigger
 
 
 def run():
-    print('Running')
-    print('Watching GPIO pin: ', settings.GPIO_INPUT_PIN)
+    logger.info('Up and running')
+    logger.info('Watching GPIO pin: %s', settings.GPIO_INPUT_PIN)
     try:
         while True:
             gevent.sleep(1)
     except KeyboardInterrupt:
-        print('Done\n')
+        logger.info('Shutting down')
         return
