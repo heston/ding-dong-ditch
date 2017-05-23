5800f346d9a34e92b6c0c4f3bd44d47c53b6759b
Heston Liebowitz <heston@matter.com>
2017-05-23 13:19:03 -0700

    Bootstrap user data when client is run

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
-from dingdongditch import trigger
+from dingdongditch import trigger, user_settings
 
 
 if __name__ == '__main__':
+    # bootstrap user data watching
+    user_settings.get_data()
     trigger.run()
