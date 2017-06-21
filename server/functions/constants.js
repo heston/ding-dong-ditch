0d2644a41be99c1b5c683f188cc0c3787fbb7098
Heston Liebowitz <heston@matter.com>
2017-06-20 21:59:35 -0700

    Play doorbell audio file when doorbell action runs

---
+++ server/functions/constants.js
@@ -0,0 +1,2 @@
+module.exports.DOORBELL_AUDIO_URL =
+    'https://ding-dong-ditch.firebaseapp.com/audio/doorbell.wav';
--- server/functions/responses/doorbell.js
+++ server/functions/responses/doorbell.js
@@ -1,5 +1,6 @@
 const TwimlResponse = require('./twiml-response');
 const { times } = require('lodash');
+const { DOORBELL_AUDIO_URL } = require('../constants');
 
 module.exports = class Doorbell extends TwimlResponse {
     constructor(baseUrl, pin) {
@@ -17,6 +18,7 @@ module.exports = class Doorbell extends TwimlResponse {
         });
 
         times(5, () => {
+            gather.play(DOORBELL_AUDIO_URL);
             gather.say(
                 'This is your doorbell calling. Someone is at the door. ' +
                 'Press any key to unlock the gate.'
