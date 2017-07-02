6b6cb64bae49a2a7023007c2aff292678f1fa0c6
Heston Liebowitz <heston@matter.com>
2017-07-01 19:55:07 -0700

    Added multithreaded watcher for stale data

---
+++ tests/test_watcher.py
@@ -0,0 +1,36 @@
+from datetime import timedelta
+import time
+
+from dingdongditch import watcher
+
+
+def test_watcher__not_stale(mocker):
+    is_stale = mocker.Mock()
+    is_stale.side_effect = [
+        False,
+        False,
+        False,
+    ]
+    update = mocker.Mock()
+    interval = timedelta(seconds=.001)
+    cancel = watcher.watch(is_stale, update, interval)
+    time.sleep(.01)
+    cancel()
+    assert is_stale.call_count == 4
+    assert update.called is False
+
+
+def test_watcher__stale(mocker):
+    is_stale = mocker.Mock()
+    is_stale.side_effect = [
+        False,
+        False,
+        True,
+    ]
+    update = mocker.Mock()
+    interval = timedelta(seconds=.001)
+    cancel = watcher.watch(is_stale, update, interval)
+    time.sleep(.01)
+    cancel()
+    assert is_stale.call_count == 4
+    assert update.called is True
