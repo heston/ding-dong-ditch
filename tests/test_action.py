58abc0bbc5cf1647098852fd8c7812918300c12a
Heston Liebowitz <hestonl@gmail.com>
2018-06-21 20:29:05 -0700

    Allow strike open duration to be customized via env var

--- tests/test_action.py
+++ tests/test_action.py
@@ -1,3 +1,5 @@
+from gpiozero import DigitalOutputDevice
+
 from dingdongditch import action
 
 
@@ -14,3 +16,35 @@ class TestStrike:
         instance1 = action.Strike.get(1)
         instance2 = action.Strike.get(1)
         assert instance1 is instance2
+
+    def test_release__default(self, mocker):
+        instance = action.Strike(1)
+        blink_mock = mocker.patch.object(DigitalOutputDevice, 'blink', create=True)
+
+        instance.release()
+
+        blink_mock.assert_called_with(on_time=3, off_time=1, n=1, background=True)
+
+    def test_release__zero(self, mocker):
+        instance = action.Strike(1)
+        blink_mock = mocker.patch.object(DigitalOutputDevice, 'blink', create=True)
+
+        instance.release(0)
+
+        blink_mock.assert_called_with(on_time=3, off_time=1, n=1, background=True)
+
+    def test_release__none(self, mocker):
+        instance = action.Strike(1)
+        blink_mock = mocker.patch.object(DigitalOutputDevice, 'blink', create=True)
+
+        instance.release(None)
+
+        blink_mock.assert_called_with(on_time=3, off_time=1, n=1, background=True)
+
+    def test_release__custom(self, mocker):
+        instance = action.Strike(1)
+        blink_mock = mocker.patch.object(DigitalOutputDevice, 'blink', create=True)
+
+        instance.release(10)
+
+        blink_mock.assert_called_with(on_time=10, off_time=1, n=1, background=True)
