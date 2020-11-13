95fb500d099fa96008adad2119b4a36709053c77
Heston Liebowitz <hestonl@gmail.com>
2020-11-12 22:23:17 -0800

    Actually make it work

--- server/functions/db/reset-strike.js
+++ server/functions/db/reset-strike.js
@@ -3,14 +3,16 @@ const { setSettings } = require('../lib/settings');
 const { UNIT_CONFIG_PATH } = require('../constants');
 
 module.exports = function resetStrike() {
-    const units = admin.database().ref(UNIT_CONFIG_PATH).once('value');
-    const pendingOperations = [];
+    return admin.database().ref(UNIT_CONFIG_PATH).once('value')
+        .then((snapshot) => {
+            const units = snapshot.val();
+            const pendingOperations = [];
+            Object.keys(units).forEach((unitId) => {
+                pendingOperations.push(
+                    setSettings(unitId, 'strike', 0)
+                );
+            });
 
-    Object.keys(units).forEach((unitId) => {
-        pendingOperations.push(
-            setSettings(unitId, 'strike', 0)
-        );
-    });
-
-    return Promise.all(pendingOperations);
+            return Promise.all(pendingOperations);
+        });
 };
